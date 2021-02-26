e2a90ad2578eab63160e42f6691d4c2f15cca139
ThiagoYellow <thiago.g.santos@outlook.com.br>
2021-02-26 16:33:53 -0300

    melhorando a usabilidade

---
+++ src/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import '../styles/global.css'
+
+import { ChallengesContext, ChallengesProvider } from '../contexts/ChallengesContext'
+
+function MyApp({ Component, pageProps }) {
+
+
+
+  return (
+    <ChallengesProvider>
+      <Component {...pageProps} />
+    </ChallengesProvider>
+  )
+}
+
+export default MyApp
